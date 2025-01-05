ba1e9456b7c9695e26e928e0467a13aee892007c
Rtyuoo <139663273+Rtyuoo@users.noreply.github.com>
2025-01-05 09:16:28 +0000

    update

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
               <li><a href="login/logout.php" id="logout-link">Logout</a></li>
             </ul>
           </li>
-          <li><a href="about/index.php">About Us</a></li>
+          <li><a href="about_us/index.php">About Us</a></li>
           
         </ul>
       </nav>
